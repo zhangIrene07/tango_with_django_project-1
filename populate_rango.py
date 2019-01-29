40e371b28b9d3915ab514917392f0fac903f871a
zhangIrene07 <2423289z@student.gla.ac.uk>
2019-01-29 23:10:49 +0000

    ch 6-Exercise

--- populate_rango.py
+++ populate_rango.py
@@ -6,6 +6,7 @@ import django
 django.setup()
 from rango.models import Category, Page
 
+
 def populate():
     # First, we will create lists of dictionaries containing the pages
     # we want to add into each category.
@@ -15,25 +16,33 @@ def populate():
 
     python_pages = [
         {"title": "Official Python Tutorial",
-        "url":"http://docs.python.org/2/tutorial/"},
+        "url":"http://docs.python.org/2/tutorial/",
+        "views": 64},
         {"title":"How to Think like a Computer Scientist",
-        "url":"http://www.greenteapress.com/thinkpython/"},
+        "url":"http://www.greenteapress.com/thinkpython/",
+        "views": 32},
         {"title":"Learn Python in 10 Minutes",
-        "url":"http://www.korokithakis.net/tutorials/python/"} ]
+        "url":"http://www.korokithakis.net/tutorials/python/",
+        "views": 32}]
 
     django_pages = [
         {"title":"Official Django Tutorial",
-        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+        "views": 32},
         {"title":"Django Rocks",
-        "url":"http://www.djangorocks.com/"},
+        "url":"http://www.djangorocks.com/",
+        "views": 16},
         {"title":"How to Tango with Django",
-        "url":"http://www.tangowithdjango.com/"} ]
+        "url":"http://www.tangowithdjango.com/",
+        "views": 16}]
 
     other_pages = [
         {"title":"Bottle",
-        "url":"http://bottlepy.org/docs/dev/"},
+        "url":"http://bottlepy.org/docs/dev/",
+        "views": 16},
         {"title":"Flask",
-        "url":"http://flask.pocoo.org"} ]
+        "url":"http://flask.pocoo.org",
+        "views": 16}]
 
     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
             "Django": {"pages": django_pages,"views": 64, "likes": 32},
@@ -52,19 +61,21 @@ def populate():
         # updated to pass through views and likes parameter
         c = add_cat(cat, cat_data["views"],cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
 
+
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url=url
-    p.views=views
+    p.url = url
+    p.views = views
     p.save()
     return p
+
 
 def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
@@ -73,7 +84,7 @@ def add_cat(name, views=0, likes=0):
     c.save()
     return c
 
-# Start execution here!
-if __name__ == '__main__':
+
+if __name__ == '__main__': # Start execution here!
     print("Starting Rango population script...")
     populate()
--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,8 @@ from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories':category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
